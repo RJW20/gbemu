536ee7b409c1e566984ac3ac37ed6913582bc2e8
RJW20 <robertjwatts84@outlook.com>
2024-12-06 19:35:26 +0000

    Add the new source files to CMakeLists and check they compile and run

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include "gameboy.hpp"
 
 int main(int argc, char** argv) {
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-
-    std::cout << "Hello world with SDL2: " << argc << std::endl;
+    GameBoy gameboy;
 
     return 0;
 }
