7e2ac5ae8302bd7cf68f7003ed3addc0d6effc18
RJW20 <robertjwatts84@outlook.com>
2024-12-07 16:49:54 +0000

    Define the Mmu class and implement the read and write functions

--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,3 +1,148 @@
 #include <iostream>
 #include <cstdint>
 #include "mmu.hpp"
+#include "cartridge/cartridge.hpp"
+#include "interrupt_manager.hpp"
+#include "timer.hpp"
+
+/* Store the values of the given pointers and initialise wram and hram to their
+ * respective sizes. */
+Mmu::Mmu(Cartridge* cart, InterruptManager* inter_manag, Timer* t) {
+    cartridge = cart;
+    wram.resize(0x2000);
+    interrupt_manager = inter_manag;
+    timer = t;
+    hram.resize(0x7F);
+}
+
+/* Read the 8 bit value stored at the given address.
+ * If necessary directs the address to the corresponding component. */
+uint8_t Mmu::read(uint16_t address) const {
+
+    if (address < 0x8000) {
+        return cartridge->read(address);
+    }
+
+    else if (0x8000 <= address && address < 0xA000) {
+        std::cerr << "Failed to read from address: " << std::hex << address <<
+            ". Video RAM currently not implemented." << std::endl;
+        return 0xFF;
+    }
+
+    else if (0xA000 <= address && address < 0xC000) {
+        std::cerr << "Failed to read from address: " << std::hex << address <<
+            ". Cartridge RAM currently not implemented." << std::endl;
+        return 0xFF;
+    }
+    
+    else if (0xC000 <= address && address < 0xE000) {
+        return wram[address - 0xC000];
+    }
+
+    else if (0xE000 <= address && address < 0xFE00) {
+        std::cerr << "Failed to read from address: " << std::hex << address <<
+            ". Echo RAM should not be accessed." << std::endl;
+        return 0xFF;
+    }
+
+    else if (0xFE00 <= address && address < 0xFEA0) {
+        std::cerr << "Failed to read from address: " << std::hex << address <<
+            ". OAM currently not implemented." << std::endl;
+        return 0xFF;
+    }
+
+    else if (0xFEA0 <= address && address < 0xFEFF) {
+        std::cerr << "Failed to read from address: " << std::hex << address <<
+            ". This section of RAM is unusable." << std::endl;
+        return 0xFF;
+    }
+
+    else if (0xFF80 <= address && address < 0xFFFE) {
+        return hram[address - 0xFF80];
+    }
+
+    else {
+        switch (address) {
+            case 0xFF04:
+                return timer->exposed_div();
+            case 0xFF05:
+                return timer->tima;
+            case 0xFF06:
+                return timer->tma;
+            case 0xFF07:
+                return timer->tac;
+            case 0xFF0F:
+                return interrupt_manager->ix;
+            case 0xFFFF:
+                return interrupt_manager->ie;
+            default:
+                std::cerr << "Failed to read from address: " << std::hex <<
+                    address << ". I/O registers not fully implemented." <<
+                    std::endl;
+                return 0xFF;
+        }
+    }
+}
+
+/* Write the given 8 bit value to the given address.
+ * If necessary directs the address to the corresponding component. */
+void Mmu::write(uint16_t address, uint8_t value) {
+
+    if (address < 0x8000) {
+        return cartridge->write(address, value);
+    }
+
+    else if (0x8000 <= address && address < 0xA000) {
+        std::cerr << "Failed to write to address: " << std::hex << address <<
+            ". Video RAM currently not implemented." << std::endl;
+    }
+
+    else if (0xA000 <= address && address < 0xC000) {
+        std::cerr << "Failed to write to address: " << std::hex << address <<
+            ". Cartridge RAM currently not implemented." << std::endl;
+    }
+    
+    else if (0xC000 <= address && address < 0xE000) {
+        wram[address - 0xC000] = value;
+    }
+
+    else if (0xE000 <= address && address < 0xFE00) {
+        std::cerr << "Failed to write to address: " << std::hex << address <<
+            ". Echo RAM should not be accessed." << std::endl;
+    }
+
+    else if (0xFE00 <= address && address < 0xFEA0) {
+        std::cerr << "Failed to write to address: " << std::hex << address <<
+            ". OAM currently not implemented." << std::endl;
+    }
+
+    else if (0xFEA0 <= address && address < 0xFEFF) {
+        std::cerr << "Failed to write to address: " << std::hex << address <<
+            ". This section of RAM is unusable." << std::endl;
+    }
+
+    else if (0xFF80 <= address && address < 0xFFFE) {
+        hram[address - 0xFF80] = value;
+    }
+
+    else {
+        switch (address) {
+            case 0xFF04:
+                timer->reset_div();
+            case 0xFF05:
+                timer->tima = value;
+            case 0xFF06:
+                timer->tma = value;
+            case 0xFF07:
+                timer->set_tac(value);
+            case 0xFF0F:
+                interrupt_manager->ix = value;
+            case 0xFFFF:
+                interrupt_manager->ie = value;
+            default:
+                std::cerr << "Failed to write to address: " << std::hex <<
+                    address << ". I/O registers not fully implemented." <<
+                    std::endl;
+        }
+    }
+}
--- src/mmu.hpp
+++ src/mmu.hpp
@@ -3,16 +3,41 @@
 
 #include <cstdint>
 #include <vector>
+#include "interrupt_manager.hpp"
+#include "timer.hpp"
+#include "cartridge/cartridge.hpp"
 
+/* Memory Management Unit
+ * Simulates an array with the following contents:
+ * - 0x0000-0x3FFF - Fixed cartridge ROM bank
+ * - 0x4000-0x7FFF - Switchable catridge ROM bank
+ * - 0x8000-0x9FFF - Video RAM
+ * - 0xA000-0xBFFF - Switchable cartridge RAM bank
+ * - 0xC000-0xDFFF - Work RAM
+ * - 0xE000-0xFDFF - Echo RAM (ignored)
+ * - 0xFE00-0xFE9F - OAM
+ * - 0xFEA0-0xFEFF - Unusable
+ * - 0xFF00-0xFF7F - I/O registers
+ * - 0xFF80-0xFFFE - High RAM
+ * - 0xFFFF - Interrupt enable flag */
 class Mmu {
 public:
-    Mmu();
+    Mmu(
+        Cartridge* cartridge,
+        InterruptManager* interrupt_manager,
+        Timer* timer
+    );
     ~Mmu() {};
 
-    uint8_t read(uint16_t address);
+    uint8_t read(uint16_t address) const;
     void write(uint16_t address, uint8_t value);
 
 private:
+    Cartridge* cartridge;
+    std::vector<uint8_t> wram;              // Work RAM
+    InterruptManager* interrupt_manager;
+    Timer* timer;
+    std::vector<uint8_t> hram;              // High RAM
 };
 
 #endif
