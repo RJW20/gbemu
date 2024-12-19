ce10ca6c32de8985cbe72092690af91fec75858e
RJW20 <robertjwatts84@outlook.com>
2024-12-19 14:08:18 +0000

    Rework timer class to work with t-cycles over m-cycles

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <cstdint>
 #include "timer.hpp"
 #include "interrupt_manager.hpp"
 
-// Initialise all components to zero and set the interrupt manager pointer.
-Timer::Timer(InterruptManager* inter_manag) {
+// Initialise all components to zero.
+void Timer::reset() {
     div = 0;
     tima = 0;
     tma = 0;
@@ -12,28 +11,26 @@ Timer::Timer(InterruptManager* inter_manag) {
 
     div_counter = 0;
     tima_counter = 0;
-
-    interrupt_manager = inter_manag;
 }
 
-// Update the timer (called every m-cycle).
+// Carry out 1 t-cycle.
 void Timer::tick() {
 
-    // Increment div every 64 = 2^6 m-cycles
+    // Increment div every 256 = 2^8 t-cycles
     div_counter += 1;
-    div += (div_counter >> 6);
-    div_counter &= 0x3F;
+    div += (div_counter >> 8);
+    div_counter &= 0xFF;
 
     // Increment tima at the frequency specified in tac
     if (timer_is_enabled()) {
         tima_counter += 1;
 
-        if (tima_counter >= tima_m_cycles()) {
+        if (tima_counter == tima_t_cycles[tac & 0x03]) {
             tima_counter = 0;
             
             if (++tima == 0) {
                 tima = tma;
-                interrupt_manager->request_interrupt(2);
+                interrupt_manager->request(InterruptType(2));
             }
             
         }
@@ -58,20 +55,4 @@ void Timer::set_tac(uint8_t value) {
 // Check if the timer is enabled.
 bool Timer::timer_is_enabled() const {
     return tac & 0x04; // Bit 2 of tac
-}
-
-// Return the number of m-cycles per tima tick.
-uint8_t Timer::tima_m_cycles() const {
-    switch (tac & 0x03) { // Bits 0–1 of tac
-        case 0x00:
-            return (uint8_t) 0xFF; // 4096 Hz
-        case 0x01:
-            return (uint8_t) 0x4; // 262144 Hz
-        case 0x10:
-            return (uint8_t) 0x10; // 65536 Hz
-        case 0x11:
-            return (uint8_t) 0x40; // 16384 Hz
-        default:
-            return 0xFF;    // Never reach here
-    }
 }
--- src/timer.hpp
+++ src/timer.hpp
@@ -4,31 +4,42 @@
 #include <cstdint>
 #include "interrupt_manager.hpp"
 
+/* Simulates the system's timer hardware by managing clock cycles, updating
+ * timer registers, and triggering interrupts at specified intervals. */
 class Timer {
 public:
+    Timer(InterruptManager* interrupt_manager) :
+        interrupt_manager(interrupt_manager) {reset();};
+    ~Timer() {};
+
     uint8_t tima;   // Timer Counter
     uint8_t tma;    // Timer Modulo
     uint8_t tac;    // Timer Control
 
-    Timer(InterruptManager* interrupt_manager);
-    ~Timer() {};
-
+    void reset();
     void tick();
     uint8_t exposed_div() const;
     void reset_div();
     void set_tac(uint8_t value);
 
 private:
+    InterruptManager* interrupt_manager;
+
     uint16_t div;   // Divider Register (only upper 8 bits are exposed)
 
-    // Counters for m-cycles
-    uint8_t div_counter;
-    uint8_t tima_counter;
+    // Counters for t-cycles
+    uint16_t div_counter;
+    uint16_t tima_counter;
 
-    InterruptManager* interrupt_manager;    // Allows interrupt requests
+    /* The number of t-cycles per tima tick dependent on the trailing 2 bits of
+     * tac (used as index of the array):
+     * - 00 - 1024 (4096 Hz)
+     * - 01 - 16 (262144 Hz)
+     * - 10 - 64 (65536 Hz)
+     * - 11 - 256 (16384 Hz) */
+    const uint16_t tima_t_cycles[4] = {0x400, 0x10, 0x40, 0x100};
 
     bool timer_is_enabled() const;
-    uint8_t tima_m_cycles() const;
 };
 
 #endif
