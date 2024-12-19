5c3385dd0bcb07151fa1e9e53f3021809a3c97d8
RJW20 <robertjwatts84@outlook.com>
2024-12-19 11:31:39 +0000

    Fix get_enabled_interrupt and implement an enum for interrupt type

--- src/interrupt_manager.cpp
+++ src/interrupt_manager.cpp
@@ -1,38 +1,30 @@
-#include <cstdint>
 #include "interrupt_manager.hpp"
 
-// Initialise all components to zero.
-InterruptManager::InterruptManager() {
+// Reset all attributes.
+void InterruptManager::reset() {
     ime = false;
     ie = 0;
     ix = 0;
 }
 
-/* Request an interrupt of the given type.
- * The value of the given bit decides the type of interrupt:
- * - 0   Vblank 
- * - 1   LCD
- * - 2   Timer 
- * - 3   Serial Link 
- * - 4   Joypad */
-void InterruptManager::request_interrupt(uint8_t bit) {
-    ix |= (1 << bit);
+// Request an interrupt of the given type.
+void InterruptManager::request_interrupt(InterruptType interruption) {
+    ix |= (1 << interruption);
 }
 
 /* Return true if interrupts are enabled and a specific interrupt is
  * requested. */
-bool InterruptManager::is_interrupt_requested() {
+bool InterruptManager::is_interrupt_requested() const {
     return ime && (ie & ix);
 }
 
 /* Return the bit corresponding to the enabled interrupt.
- * Interrupts with lower bit value with have higher priority.
+ * Interrupts with lower bit value have higher priority.
  * Returns -1 if no interrupts are enabled. */
-int InterruptManager::get_enabled_interrupt() {
-    int interrupt_bit;
-    for (int bit_pos = 0< bit_pos < 5; bit_pos++;) {
-        if (((ie & ix) >> bit_pos) & 1 == 1) {
-            return interrupt_bit;
+int InterruptManager::get_enabled_interrupt() const {
+    for (int bit_pos = 0; bit_pos < 5; bit_pos++) {
+        if (((ie & ix) >> bit_pos) & 1) {
+            return bit_pos;
         }
     }
     return -1;
--- src/interrupt_manager.hpp
+++ src/interrupt_manager.hpp
@@ -3,18 +3,35 @@
 
 #include <cstdint>
 
+// Handles enabling and disabling of hardware interrupts.
 class InterruptManager {
 public:
+    InterruptManager() {reset();};
+    ~InterruptManager() {};
+
     bool ime;    // Interrupt Master Enable
     uint8_t ie;  // Interrupt Enable Register
     uint8_t ix;  // Interrupt Flag Register
 
-    InterruptManager();
-    ~InterruptManager() {};
+    /* There are 5 types of interrupt, whose values correspond to their bit
+     * position in ie and ix, as well as the index in the handler_addresses
+     * array. */
+    enum InterruptType {
+        Vblank = 0,
+        Lcd = 1,
+        Timer = 2,
+        Serial = 3,
+        Joypad = 4
+    };
 
-    void request_interrupt(uint8_t bit);
-    bool is_interrupt_requested();
-    int get_enabled_interrupt();
+    void reset();
+    void request_interrupt(InterruptType interruption);
+    bool is_interrupt_requested()const;
+    int get_enabled_interrupt() const;
+
+private:
+    // Addresses to set reg.pc to for each corresponding interrupt
+    const uint8_t handler_addresses[5] = {0x40, 0x48, 0x50, 0x58, 0x60};
 };
 
 #endif
