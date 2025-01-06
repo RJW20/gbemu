4c6ff30d0d078abd6ee020689dcaad46567f9d6a
RJW20 <robertjwatts84@outlook.com>
2025-01-06 13:13:32 +0000

    Implement the NoMbc class for simple 32KB roms

---
+++ src/cartridge/mbc/no_mbc.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdint>
+#include <fstream>
+#include "no_mbc.hpp"
+#include "base_mbc.hpp"
+
+/* Return the 8 bit value in the ROM at the given address.
+ * Returns 0xFF if the address is invalid. */
+uint8_t NoMbc::read_rom(uint16_t address) const {
+
+    if (address < ROM_BANK_SIZE) {
+        return rom[0][address];
+    }
+
+    else if (address < 2 * ROM_BANK_SIZE) {
+        return rom[1][address & (ROM_BANK_SIZE - 1)];
+    }
+    
+    else {
+        std::cerr << "Invalid NoMBC ROM read at address " << std::hex
+            << address << " - out of bounds." << std::endl;
+        return 0xFF;
+    }
+}
+
+// Does nothing.
+void NoMbc::write_rom(uint16_t address, uint8_t value) {
+    std::cerr << "Invalid NoMBC ROM write." << std::endl;
+}
+
+// Return 0xFF.
+uint8_t NoMbc::read_ram(uint16_t address) const {
+    std::cerr << "Invalid NoMBC RAM read - there are no external RAM banks."
+        << std::endl;
+    return 0xFF;
+}
+
+// Does nothing.
+void NoMbc::write_ram(uint16_t address, uint8_t value) {
+    std::cerr << "Invalid NoMBC RAM write - there are no external RAM banks."
+        << std::endl;
+}
---
+++ src/cartridge/mbc/no_mbc.hpp
@@ -0,0 +1,22 @@
+#ifndef NO_MBC_HPP
+#define NO_MBC_HPP
+
+#include <cstdint>
+#include <fstream>
+#include "base_mbc.hpp"
+
+/* A class for cartridges with no Memory Bank Controller.
+ * Used for small games with up to 32KB ROM.
+ * Directly maps the ROM to 0x0000 - 0x7FFF. */
+class NoMbc : public BaseMbc {
+public:
+    NoMbc(std::ifstream& rom_file) : BaseMbc(rom_file, false) {};
+    ~NoMbc() override {};
+
+    uint8_t read_rom(uint16_t address) const override;
+    void write_rom(uint16_t address, uint8_t value) override;
+    uint8_t read_ram(uint16_t address) const override;
+    void write_ram(uint16_t address, uint8_t value) override;
+};
+
+#endif
