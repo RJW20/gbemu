63fb62772d446dd836108099c8d4506d14cd81aa
RJW20 <robertjwatts84@outlook.com>
2025-01-06 12:33:28 +0000

    Write MBC1 class

---
+++ src/cartridge/mbc/mbc1.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <cstdint>
+#include <fstream>
+#include "mbc1.hpp"
+#include "base_mbc.hpp"
+
+// Construct BaseMbc and set the registers to their default values.
+Mbc1::Mbc1(std::ifstream& rom_file, bool has_external_ram) : 
+    BaseMbc(rom_file, has_external_ram) {
+    reset();
+}
+
+// Set registers to their default power-on values.
+void Mbc1::reset() {
+    is_ram_enabled = false;
+    rom_bank_number = 1;
+    ram_bank_number = 0;
+    advanced_banking = false;
+}
+
+/* Return the 8 bit value in the currently selected ROM bank at the given
+ * address.
+ * Only uses the trailing 14 bits of the address.
+ * Returns 0xFF if the address is invalid. */
+uint8_t Mbc1::read_rom(uint16_t address) const {
+
+    if (address < ROM_BANK_SIZE) {
+        uint8_t selected_rom_bank = advanced_banking ? 
+            (ram_bank_number << 5) & (rom_size - 1) : 0;
+        return rom[selected_rom_bank][address & (ROM_BANK_SIZE - 1)];
+    }
+
+    else if (address < 2 * ROM_BANK_SIZE) {
+        uint8_t selected_rom_bank =
+            ((ram_bank_number << 5) | rom_bank_number) & (rom_size - 1);
+        return rom[selected_rom_bank][address & (ROM_BANK_SIZE - 1)];
+    }
+    
+    else {
+        std::cerr << "Invalid MBC ROM read at address " << std::hex << address
+            << " - out of bounds." << std::endl;
+        return 0xFF;
+    }
+}
+
+/* Modify the registers depending on the region the given address falls in and
+ * the given value as follows:
+ * - 0x0000-0x1FFF - is_ram_enabled
+ * Set true if the trailing 4 bits of value are 0xA, otherwise false.
+ * - 0x2000-0x3FFF - rom_bank_number
+ * Set to the trailing 5 bits of value. If set to 0 is incremented to 1.
+ * - 0x4000-0x5FFF - ram_bank_number
+ * Set to the trailing 2 bits of value.
+ * - 0x6000-0x7FFF - advanced_banking
+ * Set to the trailing bit of value. */
+void Mbc1::write_rom(uint16_t address, uint8_t value) {
+
+    if (address < IS_RAM_ENABLED_UPPER) {
+        is_ram_enabled = ((value & 0xF) == 0xA);
+    }
+
+    else if (address < ROM_BANK_SIZE) {
+        rom_bank_number = value & 0b11111;
+        rom_bank_number = rom_bank_number ? rom_bank_number : 1;
+    }
+
+    else if (address < RAM_BANK_NUMBER_UPPER) {
+        ram_bank_number = value & 0b11;
+    }
+
+    else if (address < 2 * ROM_BANK_SIZE) {
+        advanced_banking = value & 1;
+    }
+    
+    else {
+        std::cerr << "Invalid MBC ROM write at address " << std::hex << address
+            << " - out of bounds." << std::endl;
+    }
+}
+
+/* Return the 8 bit value in the currently selected RAM bank at the given
+ * address.
+ * Only uses the trailing 14 bits of the address.
+ * Returns 0xFF if there are no external RAM banks, if RAM is not accessible or
+ * if the address is invalid. */
+uint8_t Mbc1::read_ram(uint16_t address) const {
+
+    if (!ram_size) {
+        std::cerr << "Invalid MBC RAM read - there are no external RAM banks."
+            << std::endl;
+        return 0xFF;
+    }
+
+    if (!is_ram_enabled) {
+        std::cerr << "Invalid MBC RAM read - external RAM is currently not "
+            << "accessible." << std::endl;
+        return 0xFF;
+    }
+
+    if (address < RAM_BANK_LOWER || address >= RAM_BANK_LOWER + RAM_BANK_SIZE) {
+        std::cerr << "Invalid MBC RAM read at address " << std::hex << address
+            << " - out of bounds." << std::endl;
+        return 0xFF;
+    }
+
+    uint8_t selected_ram_bank = advanced_banking ? ram_bank_number : 0;
+    return ram[selected_ram_bank][address & (RAM_BANK_SIZE - 1)];
+}
+
+/* Write the given 8 bit value to the given address in the currently selected
+ * RAM bank.
+ * Only uses the trailing 14 bits of the address.
+ * Fails if there are no external RAM banks, if RAM is not accessible or if the
+ * address is invalid. */
+void Mbc1::write_ram(uint16_t address, uint8_t value) {
+
+    if (!ram_size) {
+        std::cerr << "Invalid MBC RAM write - there are no external RAM banks."
+            << std::endl;
+        return;
+    }
+
+    if (!is_ram_enabled) {
+        std::cerr << "Invalid MBC RAM write - external RAM is currently not "
+            << "accessible." << std::endl;
+        return;
+    }
+
+    if (address < RAM_BANK_LOWER || address >= RAM_BANK_LOWER + RAM_BANK_SIZE) {
+        std::cerr << "Invalid MBC RAM write at address " << std::hex << address
+            << " - out of bounds." << std::endl;
+        return;
+    }
+
+    uint8_t selected_ram_bank = advanced_banking ? ram_bank_number : 0;
+    ram[selected_ram_bank][address & (RAM_BANK_SIZE - 1)] = value;
+}
---
+++ src/cartridge/mbc/mbc1.hpp
@@ -0,0 +1,34 @@
+#ifndef MBC1_HPP
+#define MBC1_HPP
+
+#include <cstdint>
+#include <fstream>
+#include "base_mbc.hpp"
+
+// Memory Bank Controller version 1.
+class Mbc1 : public BaseMbc {
+public:
+    Mbc1(std::ifstream& rom_file, bool has_external_ram);
+    ~Mbc1() override {};
+
+    void reset() override;
+
+    uint8_t read_rom(uint16_t address) const override;
+    void write_rom(uint16_t address, uint8_t value) override;
+    uint8_t read_ram(uint16_t address) const override;
+    void write_ram(uint16_t address, uint8_t value) override;
+
+private:
+    // Registers
+    bool is_ram_enabled;        // Controls if RAM is accessible
+    uint8_t rom_bank_number;    // Currently selected ROM bank (5 bit)
+    uint8_t ram_bank_number;    // Currently selected RAM bank (2 bit)
+    bool advanced_banking;      // Controls behaviour of ram_bank_number
+
+    // Write regions
+    static constexpr size_t IS_RAM_ENABLED_UPPER = 0x2000;
+    static constexpr size_t RAM_BANK_NUMBER_UPPER = 0x6000;
+    static constexpr size_t RAM_BANK_LOWER = 0xA000;
+};
+
+#endif
