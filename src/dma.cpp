b37121865f389dc5b053119a275457d60201a271
RJW20 <robertjwatts84@outlook.com>
2025-01-12 15:13:21 +0000

    Implement the direct memory access class

---
+++ src/dma.cpp
@@ -0,0 +1,45 @@
+#include <cstdint>
+#include "dma.hpp"
+
+// Set _source_address to it's power-on value. 
+void Dma::reset() {
+    _source_address = 0xFF;
+    transfer_in_progress = false;
+}
+
+// Carry out 1 t-cycle.
+void Dma::tick() {
+
+    if (!transfer_in_progress) {
+        return;
+    }
+
+    // Only act every 4 t-cycles
+    if (++locked < 4) {
+        return;
+    }
+    locked = 0;
+
+    mmu->write(
+        OAM_ADDRESS + current_m_cycles,
+        mmu->read(_source_address + current_m_cycles)
+    );
+    
+    if (++current_m_cycles == TOTAL_TRANSFER_M_CYCLES) {
+        transfer_in_progress = false;
+    }
+
+}
+
+// Return _source_address >> 8.
+uint8_t Dma::source_address() {
+    return _source_address >> 8;
+}
+
+// Set _source_address to the given value << 8 and start a transfer.
+void Dma::set_source_address(uint8_t value) {
+    _source_address = value << 8;
+    transfer_in_progress = true;
+    locked = 0;
+    current_m_cycles = 0;
+}
---
+++ src/dma.hpp
@@ -0,0 +1,35 @@
+#ifndef DMA_HPP
+#define DMA_HPP
+
+#include <cstdint>
+#include "mmu.hpp"
+
+/* Direct Memory Access (DMA)
+ * Enables fast copying of data to the OAM removing responsibility from the
+ * main program. */
+class Dma {
+public:
+    Dma(Mmu* mmu) : mmu(mmu) { reset(); };
+    ~Dma() {};
+
+    void reset();
+    void tick();
+
+    bool transfer_in_progress;
+
+    uint8_t source_address();
+    void set_source_address(uint8_t value);
+
+private:
+    Mmu* mmu;
+
+    uint8_t locked;
+    uint8_t current_m_cycles;
+    static constexpr uint16_t TOTAL_TRANSFER_M_CYCLES = 160;
+
+    // The 16 bit address to start the transfer from
+    uint8_t _source_address;
+    static constexpr uint16_t OAM_ADDRESS = 0xFE00;
+};
+
+#endif
