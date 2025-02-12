58f20e6be33171d7c4056c900b41f97329478563
RJW20 <robertjwatts84@outlook.com>
2025-02-12 15:48:08 +0000

    Move most register method definitions into the header file

--- src/cpu/register.cpp
+++ src/cpu/register.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include <cstdint>
 #include "register.hpp"
 
-// Set all register values to their default GameBoy initial values
+// Set registers to their post boot ROM values.
 void Register::reset() {
     set_af(0x01B0);
     set_bc(0x0013);
@@ -27,64 +26,8 @@ uint8_t Register::f() {
  * Outputs to std::cerr if the trailing 4 bits in the given value are non-
  * zero. */
 void Register::set_f(uint8_t value) {
-    if (value & 0xF) {
-        std::cerr << "The trailing 4 bits in register f must always be zero, "
-            "ignoring them in value: " << std::hex << value << std::endl;
-    }
     flag_z = value >> 7;
     flag_n = (value >> 6) & 1;
     flag_h = (value >> 5) & 1;
     flag_c = (value >> 4) & 1;
-}
-
-/* Return a 16 bit integer made up of register a as the first byte and register
- * f as the second byte. */
-uint16_t Register::af() {
-    return (uint16_t) (a << 8) | f();
-}
-
-/* Return a 16 bit integer made up of register b as the first byte and register
- * c as the second byte. */
-uint16_t Register::bc() {
-    return (uint16_t) (b << 8) | c;
-}
-
-/* Return a 16 bit integer made up of register d as the first byte and register
- * e as the second byte. */
-uint16_t Register::de() {
-    return (uint16_t) (d << 8) | e;
-}
-
-/* Return a 16 bit integer made up of register h as the first byte and register
- * l as the second byte. */
-uint16_t Register::hl() {
-    return (uint16_t) (h << 8) | l;
-}
-
-/* Write the first byte of the given value to register a and the first 4 bits
- * of the second byte to register f. */
-void Register::set_af(uint16_t value) {
-    a = value >> 8;
-    set_f((uint8_t) value);
-}
-
-/* Write the first byte of the given value to register b and the second byte to
- * register c. */
-void Register::set_bc(uint16_t value) {
-    b = value >> 8;
-    c = value & 0xFF;
-}
-
-/* Write the first byte of the given value to register d and the second byte to
- * register e. */
-void Register::set_de(uint16_t value) {
-    d = value >> 8;
-    e = value & 0xFF;
-}
-
-/* Write the first byte of the given value to register h and the second byte to
- * register l. */
-void Register::set_hl(uint16_t value) {
-    h = value >> 8;
-    l = value & 0xFF;
 }
--- src/cpu/register.hpp
+++ src/cpu/register.hpp
@@ -3,12 +3,14 @@
 
 #include <cstdint>
 
-/* Simulates 8 8-bit registers that hold data that can be manipulated by the
- * cpu when carrying out instructions. */
+/* Register
+ * Simulates 8 8-bit registers that hold data that can be manipulated by the
+ * CPU when carrying out instructions. */
 class Register {
 public:
-    Register() {reset();};
-    ~Register() {};
+    Register() { reset(); }
+
+    void reset();
 
     uint8_t a;      // Accumulator
     uint8_t b, c;   // General-purpose registers
@@ -19,25 +21,23 @@ public:
 
     // Flags register
     uint8_t f();
-    void set_f(uint8_t);
-    bool flag_z;  // Zero flag
-    bool flag_n;  // Subtract flag
-    bool flag_h;  // Half-carry flag
-    bool flag_c;  // Carry flag
-
-    void reset();
+    void set_f(uint8_t value);
+    bool flag_z : 1;  // Zero flag
+    bool flag_n : 1;  // Subtract flag
+    bool flag_h : 1;  // Half-carry flag
+    bool flag_c : 1;  // Carry flag
 
     // 16 bit getters
-    uint16_t af();
-    uint16_t bc();
-    uint16_t de();
-    uint16_t hl();
+    uint16_t af() { return (a << 8) | f(); }
+    uint16_t bc() { return (b << 8) | c; }
+    uint16_t de() { return (d << 8) | e; }
+    uint16_t hl() { return (h << 8) | l; }
 
     // 16 bit setters
-    void set_af(uint16_t value);
-    void set_bc(uint16_t value);
-    void set_de(uint16_t value);
-    void set_hl(uint16_t value);
+    void set_af(uint16_t value) { a = value >> 8; set_f(value & 0xFF); }
+    void set_bc(uint16_t value) { b = value >> 8; c = value & 0xFF; }
+    void set_de(uint16_t value) { d = value >> 8; e = value & 0xFF; }
+    void set_hl(uint16_t value) { h = value >> 8; l = value & 0xFF; }
 };
 
 #endif
