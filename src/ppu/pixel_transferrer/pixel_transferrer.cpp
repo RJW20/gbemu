b9ff9cd39e818d32e3099b60ee2fee488032f102
RJW20 <robertjwatts84@outlook.com>
2025-03-06 10:02:32 +0000

    Fix ppu pixel shifting when bgwin and objects fifos are both non-empty

--- src/ppu/pixel_transferrer/pixel_transferrer.cpp
+++ src/ppu/pixel_transferrer/pixel_transferrer.cpp
@@ -308,9 +308,15 @@ void PixelTransferrer::shift_pixel() {
     }
     else {
         const Pixel object_pixel = object_fifo.shift();
-        shifted_pixel = !bgwin_pixel.colour_id ? object_pixel :
-            !object_pixel.colour_id || object_pixel.priority ?
-            bgwin_pixel : object_pixel;
+        if (!object_pixel.colour_id) {
+            shifted_pixel = bgwin_pixel;
+        }
+        else if (object_pixel.priority && bgwin_pixel.colour_id) {
+            shifted_pixel = bgwin_pixel;
+        }
+        else {
+            shifted_pixel = object_pixel;
+        }
     }
     pixel_buffer[ly_ * SCREEN_WIDTH + lx++] = pixel_to_rgba(shifted_pixel);
 }
